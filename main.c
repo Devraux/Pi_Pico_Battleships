24380160d0532a897b98782e42b2dd3cd21e91a8
Devraux <krzysztof.plonka2@onet.pl>
2025-11-29 22:52:25 +0100

    dhcp new functionalites

--- main.c
+++ main.c
@@ -1,18 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
+#include "gameServer.h"
 
 #define LED_PIN 0
 
 int main()
 {
-    cyw43_arch_init();
-    stdio_init_all();
-    
 
+    stdio_init_all();
+    gameServerInit();
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
-    while(true)
+
+
+    gameMatchEnemies();
+
+    while (true)
     {
         printf("blinking!!!\r\n");
         cyw43_arch_gpio_put(LED_PIN, true);
